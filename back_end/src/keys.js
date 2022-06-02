2d562080d732943576a7870884eb1dbec591afe1
= <=>
2022-06-01 22:46:36 -0700

    hello

--- back_end/src/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    MongodbUrl: 'mongodb+srv://vivek:12345@cluster0.5u0mi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    user: 'rayvivek881@gmail.com',
-    pass: '1221344V',
-}
